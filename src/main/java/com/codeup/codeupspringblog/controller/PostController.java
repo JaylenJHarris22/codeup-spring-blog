13b267d3d74f5fb49e3cabf25427ed27c7512870
Jaylen Harris <harris.j.jaylen22@gmail.com>
2023-06-05 16:25:30 -0500

    finished the exercise

--- src/main/java/com/codeup/codeupspringblog/controller/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controller/PostController.java
@@ -1,22 +1,35 @@
 package com.codeup.codeupspringblog.controller;
 
+import com.codeup.codeupspringblog.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
     @GetMapping("/posts")
-    @ResponseBody
-    public String index(){
-        return "Ole glory";
+    public String index(Model model){
+        List<Post> posts = new ArrayList<>();
+        Post p1 = new Post("Cats", "Cats be cool");
+        Post p2 = new Post("Dogs", "Dogs be cool");
+        posts.add(p1);
+        posts.add(p2);
+
+        model.addAttribute("posts", posts);
+        return "/post/index";
     }
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String specificPost(@PathVariable long id){
-        return "I'm specific " + id;
+    public String specificPost(@PathVariable long id, Model model){
+        Post p1 = new Post("Cats", "Cats be cool");
+
+        model.addAttribute("post", p1);
+        return "/post/show";
     }
     @GetMapping("/posts/create")
     @ResponseBody
