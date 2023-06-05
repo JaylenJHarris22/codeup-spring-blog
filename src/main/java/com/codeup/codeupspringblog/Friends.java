e8991c99a67e4f3ed27d662fb0b74e4f165f71a8
Jaylen Harris <harris.j.jaylen22@gmail.com>
2023-06-05 15:54:13 -0500

    problem 1

---
+++ src/main/java/com/codeup/codeupspringblog/Friends.java
@@ -0,0 +1,15 @@
+package com.codeup.codeupspringblog;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Friends {
+    private String name;
+    private int age;
+}
--- src/main/java/com/codeup/codeupspringblog/controller/HomeController.java
+++ src/main/java/com/codeup/codeupspringblog/controller/HomeController.java
@@ -1,8 +1,15 @@
 package com.codeup.codeupspringblog.controller;
 
+import com.codeup.codeupspringblog.Friends;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Controller
 public class HomeController {
@@ -10,5 +17,46 @@ public class HomeController {
     @ResponseBody
     public String landingPage(){
         return "This is a landing page";
+    }
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+
+//    @GetMapping("/home/{name}")
+//    public String homeName(@PathVariable String name, Model model){
+//        model.addAttribute("name", name);
+//
+//        return "home";
+//    }
+
+    @GetMapping("/home/{user}")
+    public String userPage(@PathVariable String user, Model model){
+        Friends friend = new Friends("Lily", 22);
+        Friends friend2 = new Friends("Jame", 18);
+        Friends friend3 = new Friends("Thomas", 30);
+        List<Friends> friendsList = new ArrayList<>();
+        friendsList.add(friend);
+        friendsList.add(friend2);
+        friendsList.add(friend3);
+
+        model.addAttribute("user", user);
+        model.addAttribute("friends", friendsList);
+        return "home";
+    }
+
+    @GetMapping("/roll-dice")
+    public String diceRoller(){
+        return "dice/dice-roller";
+    }
+
+    @GetMapping("/roll-dice/{num}")
+    public String diceResult(@PathVariable int num, Model model){
+        Random random = new Random();
+        int rando = random.nextInt(6) + 1;
+
+        model.addAttribute("userNum", num);
+        model.addAttribute("random", rando);
+        return "dice/dice-result";
     }
 }
