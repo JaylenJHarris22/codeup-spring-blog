481016b368e4b84bd9ff5ecbb1a1dce058b8aa0c
Jaylen Harris <harris.j.jaylen22@gmail.com>
2023-06-22 23:49:48 -0500

    sibling repo

---
+++ src/main/java/com/codeup/codeupspringblog/repositories/SiblingRepository.java
@@ -0,0 +1,7 @@
+package com.codeup.codeupspringblog.repositories;
+
+import com.codeup.codeupspringblog.models.Sibling;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SiblingRepository extends JpaRepository<Sibling, Long> {
+}
